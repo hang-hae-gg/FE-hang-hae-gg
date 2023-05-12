6ce48987ab618abca79aa2bfaa420cdd69c9b635
HaeJinS2 <9674qzq@gmail.com>
2023-05-12 22:38:00 +0900

    feat: Add useEffect to manage logined state

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,31 @@
 import Router from "./shared/Router";
 import "./index.css";
+import { setIsAuthenticated, setUserId } from "./redux/modules/authReducer";
+import { useEffect } from "react";
+import { useDispatch } from 'react-redux';
+import Cookies from 'js-cookie';
+import jwt_decode from "jwt-decode";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect = (() => {
+
+    // 쿠키에서 토큰 가져와서 저장
+    const token = Cookies.get('token')
+    if (token) {
+      try {
+        const decoded = jwt_decode(token)
+        // 일반로그인시 decoded.email 아닐수도 !
+        dispatch(setUserId(decoded.email))
+        dispatch(setIsAuthenticated(true))
+      } catch (error) {
+        console.error("토큰 오류", error)
+        dispatch(setIsAuthenticated(false))
+      }
+    }
+  }, [dispatch])
+
   return (
     <div>
       <Router />
