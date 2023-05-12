6830299b56ecfa849a1f3df395dc3e0cea87da03
HaeJinS2 <9674qzq@gmail.com>
2023-05-12 16:30:47 +0900

    init: Add tailwindcss

--- client/src/pages/Join.jsx
+++ client/src/pages/Join.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import '../index.css';
 
 function Join() {
   return (
-    <div>Join</div>
+    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+      <div className="flex-shrink-0">
+        <img className="h-12 w-12" src="/logo.svg" alt="ChitChat Logo"/>
+      </div>
+      <div>
+        <div className="text-xl font-medium text-blue-100">ChitChat</div>
+        <p className="text-gray-500">You have a new message!</p>
+      </div>
+    </div>
   )
 }
 
