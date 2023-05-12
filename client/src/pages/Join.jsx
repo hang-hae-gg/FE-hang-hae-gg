1681d1eed06359a1b6d9ebc98f97d49c7d70d4b9
HaeJinS2 <9674qzq@gmail.com>
2023-05-12 17:00:06 +0900

    design: Add css to join page

--- client/src/pages/Join.jsx
+++ client/src/pages/Join.jsx
@@ -3,13 +3,23 @@ import '../index.css';
 
 function Join() {
   return (
-    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-      <div className="flex-shrink-0">
-        <img className="h-12 w-12" src="/logo.svg" alt="ChitChat Logo"/>
-      </div>
-      <div>
-        <div className="text-xl font-medium text-blue-100">ChitChat</div>
-        <p className="text-gray-500">You have a new message!</p>
+
+    <div className='flex flex-1 items-center justify-center py-16 bg-[#F5F5F5] h-screen '>
+      <div className="max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center w-[700px] h-[600px] px-10">
+        <div className='flex flex-1 flex-col  items-center'>
+          <h1 className='text-[50px] font-semibold'>HH.GG</h1>
+          <div>
+            <input
+              className='bg-transparent border-b py-3 mt-10 outline-none w-full focus:border-black' placeholder="ID를 입력하세요">
+            </input>
+            <input
+              className='bg-transparent border-b py-3 mt-10 outline-none w-full focus:border-black' placeholder="PASSWORD를 입력하세요">
+            </input>
+          </div>
+          <div>
+            <button className='mt-12 w-[100px] h-[50px]'>회원가입</button>
+          </div>
+        </div>
       </div>
     </div>
   )
