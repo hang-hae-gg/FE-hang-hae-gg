65fd0ea1e48bff7781d218f80587dc0e47ba7e40
HaeJinS2 <9674qzq@gmail.com>
2023-05-13 14:04:04 +0900

    feat: Add conditional rendering in Header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,25 +1,42 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux';
+import logout from '../api/logout';
+
 
 function Header() {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const isAuthenticated = useSelector((state) => state.auth.isAuthenticated)
+    const userId = useSelector((state) => state.auth.userId)
+    console.log(isAuthenticated)
     return (
         <div className='flex justify-center'>
             <div className='flex flex-1 max-w-[1200px] w-full bg-[#ffffff] h-[65px]  '>
                 <div className='flex flex-1 justify-between mx-10 items-center'>
                     <div className='flex'>
-                        <h1 className='text-[30px] text-[#FFD602] font-bold'>HH.GG</h1>
-                    </div>
-                    <div className='flex gap-10'>
                         <button
                             onClick={() => navigate(`/`)}
-                            className='text-[#000000]'>홈</button>
+                            className='text-[30px] text-[#FFD602] font-bold'>HH.GG</button>
+                    </div>
+                    <div className='flex gap-10'>
+                        {
+                            isAuthenticated && <span>{userId}님 환영합니다!</span>
+                        }
                         <button
                             onClick={() => navigate(`/`)}
                             className='text-[#000000]'>커뮤니티</button>
-                        <button
-                            onClick={() => navigate(`/login`)}
-                            className='text-[#000000]'>로그인</button>
+
+                        {
+                            isAuthenticated ?
+                                <button
+                                    onClick={() => logout(dispatch)}
+                                    className='text-[#000000]'>로그아웃</button>
+                                :
+                                <button
+                                    onClick={() => navigate(`/login`)}
+                                    className='text-[#000000]'>로그인</button>
+                        }
                     </div>
                 </div>
             </div>
