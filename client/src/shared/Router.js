d26eed2c9320c646922db8248bb73b18ccd7569a
Sejun0910 <choisejun99@gmail.com>
2023-05-13 21:39:24 +0900

    fix: git action 오류 수정

--- client/src/shared/Router.js
+++ client/src/shared/Router.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "../pages/Login";
-import Join from "../pages/Join";
 import { Main } from "../pages/Main";
 import { ScoreSearch } from "../pages/ScoreSearch";
 import Detail from "../pages/Detail";
