03210a522a75658208ccfbd8393ae26840bf0390
Sejun0910 <choisejun99@gmail.com>
2023-05-16 00:29:30 +0900

    feat : api 미완성이라 response로 테스트 fix : axios 오류 수정

---
+++ client/src/scoresearchData.js
@@ -0,0 +1,218 @@
+const scoresearchData = {
+  status: "OK",
+  message: "검색완료",
+  data: {
+    tier: "GRANDMASTER",
+    rank: "I",
+    summonerName: "Hide on bush",
+    leaguePoints: 881,
+    summonerIconUrl:
+      "https://ddragon.leagueoflegends.com/cdn/13.9.1/img/profileicon/6.png",
+    wins: 272,
+    losses: 243,
+    matchResults: [
+      {
+        gametype: "랭크게임",
+        winresult: true,
+        championName: "Kennen",
+        championImg:
+          "https://ddragon.leagueoflegends.com/cdn/13.9.1/img/champion/Kennen.png",
+        kills: 10,
+        deaths: 3,
+        assists: 6,
+      },
+      {
+        gametype: "랭크게임",
+        winresult: true,
+        championName: "KSante",
+        championImg:
+          "https://ddragon.leagueoflegends.com/cdn/13.9.1/img/champion/KSante.png",
+        kills: 3,
+        deaths: 2,
+        assists: 4,
+      },
+      {
+        gametype: "랭크게임",
+        winresult: true,
+        championName: "Nautilus",
+        championImg:
+          "https://ddragon.leagueoflegends.com/cdn/13.9.1/img/champion/Nautilus.png",
+        kills: 0,
+        deaths: 2,
+        assists: 13,
+      },
+      {
+        gametype: "랭크게임",
+        winresult: true,
+        championName: "Leblanc",
+        championImg:
+          "https://ddragon.leagueoflegends.com/cdn/13.9.1/img/champion/Leblanc.png",
+        kills: 2,
+        deaths: 4,
+        assists: 3,
+      },
+      {
+        gametype: "랭크게임",
+        winresult: true,
+        championName: "Ahri",
+        championImg:
+          "https://ddragon.leagueoflegends.com/cdn/13.9.1/img/champion/Ahri.png",
+        kills: 9,
+        deaths: 3,
+        assists: 17,
+      },
+      {
+        gametype: "랭크게임",
+        winresult: false,
+        championName: "Darius",
+        championImg:
+          "https://ddragon.leagueoflegends.com/cdn/13.9.1/img/champion/Darius.png",
+        kills: 4,
+        deaths: 5,
+        assists: 0,
+      },
+      {
+        gametype: "랭크게임",
+        winresult: true,
+        championName: "Khazix",
+        championImg:
+          "https://ddragon.leagueoflegends.com/cdn/13.9.1/img/champion/Khazix.png",
+        kills: 7,
+        deaths: 2,
+        assists: 3,
+      },
+      {
+        gametype: "랭크게임",
+        winresult: true,
+        championName: "Azir",
+        championImg:
+          "https://ddragon.leagueoflegends.com/cdn/13.9.1/img/champion/Azir.png",
+        kills: 7,
+        deaths: 1,
+        assists: 11,
+      },
+      {
+        gametype: "랭크게임",
+        winresult: false,
+        championName: "Nautilus",
+        championImg:
+          "https://ddragon.leagueoflegends.com/cdn/13.9.1/img/champion/Nautilus.png",
+        kills: 8,
+        deaths: 6,
+        assists: 7,
+      },
+      {
+        gametype: "랭크게임",
+        winresult: false,
+        championName: "KogMaw",
+        championImg:
+          "https://ddragon.leagueoflegends.com/cdn/13.9.1/img/champion/KogMaw.png",
+        kills: 4,
+        deaths: 11,
+        assists: 5,
+      },
+      {
+        gametype: "랭크게임",
+        winresult: true,
+        championName: "Ornn",
+        championImg:
+          "https://ddragon.leagueoflegends.com/cdn/13.9.1/img/champion/Ornn.png",
+        kills: 4,
+        deaths: 3,
+        assists: 2,
+      },
+      {
+        gametype: "랭크게임",
+        winresult: true,
+        championName: "Ornn",
+        championImg:
+          "https://ddragon.leagueoflegends.com/cdn/13.9.1/img/champion/Ornn.png",
+        kills: 5,
+        deaths: 6,
+        assists: 19,
+      },
+      {
+        gametype: "랭크게임",
+        winresult: true,
+        championName: "Fiora",
+        championImg:
+          "https://ddragon.leagueoflegends.com/cdn/13.9.1/img/champion/Fiora.png",
+        kills: 3,
+        deaths: 2,
+        assists: 6,
+      },
+      {
+        gametype: "랭크게임",
+        winresult: true,
+        championName: "Ahri",
+        championImg:
+          "https://ddragon.leagueoflegends.com/cdn/13.9.1/img/champion/Ahri.png",
+        kills: 11,
+        deaths: 2,
+        assists: 9,
+      },
+      {
+        gametype: "랭크게임",
+        winresult: false,
+        championName: "Lissandra",
+        championImg:
+          "https://ddragon.leagueoflegends.com/cdn/13.9.1/img/champion/Lissandra.png",
+        kills: 1,
+        deaths: 3,
+        assists: 0,
+      },
+      {
+        gametype: "랭크게임",
+        winresult: true,
+        championName: "Tristana",
+        championImg:
+          "https://ddragon.leagueoflegends.com/cdn/13.9.1/img/champion/Tristana.png",
+        kills: 4,
+        deaths: 2,
+        assists: 2,
+      },
+      {
+        gametype: "랭크게임",
+        winresult: true,
+        championName: "Kennen",
+        championImg:
+          "https://ddragon.leagueoflegends.com/cdn/13.9.1/img/champion/Kennen.png",
+        kills: 9,
+        deaths: 3,
+        assists: 5,
+      },
+      {
+        gametype: "랭크게임",
+        winresult: false,
+        championName: "Nidalee",
+        championImg:
+          "https://ddragon.leagueoflegends.com/cdn/13.9.1/img/champion/Nidalee.png",
+        kills: 10,
+        deaths: 10,
+        assists: 13,
+      },
+      {
+        gametype: "랭크게임",
+        winresult: false,
+        championName: "Yone",
+        championImg:
+          "https://ddragon.leagueoflegends.com/cdn/13.9.1/img/champion/Yone.png",
+        kills: 6,
+        deaths: 4,
+        assists: 10,
+      },
+      {
+        gametype: "랭크게임",
+        winresult: false,
+        championName: "Azir",
+        championImg:
+          "https://ddragon.leagueoflegends.com/cdn/13.9.1/img/champion/Azir.png",
+        kills: 1,
+        deaths: 4,
+        assists: 1,
+      },
+    ],
+  },
+};
+
+export default scoresearchData;
