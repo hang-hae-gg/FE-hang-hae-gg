6bf23990bc19e9efbd1733096e937b23bbf3b005
Sejun0910 <choisejun99@gmail.com>
2023-05-18 16:28:36 +0900

    feat : 커뮤니티 UI완성

--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -1,17 +1,28 @@
-import Form from '../components/Form';
+import Form from "../components/Form";
+import Styled, { styled } from "styled-components";
 
 function Community() {
-
-    return (
-        <div >
-            <div className="bg-[#5383e8] border-t border-white text-white py-4">
-                <div className="text-5xl font-bold">커뮤니티</div>
-                <div>함께 게임을 즐길 친구를 찾아보세요!</div>
-            </div>
-            <Form />
-        </div>
-    );
-};
+  return (
+    <div>
+      <CommunityDiv>
+        <h1>커뮤니티</h1>
+      </CommunityDiv>
+      <Form />
+    </div>
+  );
+}
 
 export default Community;
 
+const CommunityDiv = styled.div`
+  background-color: white;
+  color: black;
+  border-bottom: 1px solid #ebeef1;
+  h1 {
+    width: 1080px;
+    margin: 0px auto;
+    font-size: 24px;
+    padding: 15px 0px;
+    font-weight: bold;
+  }
+`;
